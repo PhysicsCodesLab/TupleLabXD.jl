90c85f2fd271fe6e14afe4386f244818556e7d1d
PhysicsCodesLab <physics.codes@gmail.com>
2021-06-10 11:31:18 +0200

    initialize

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,14 +1,56 @@
-```@meta
-CurrentModule = TupleToolsXD
+# TupleToolsXD.jl
+
+TupleToolsXD contains a bunch of tools for using tuples (mostly homogeneous tuples `NTuple{N}`) as a collection and performing a number of operations with an inferrable result, typically also an `NTuple{M}` with inferable length `M`. Chosen implementations are typically faster than the corresponding functions in base for those small tuple lengths, but can be slower for larger tuples. Inference breaks down for most functions in case of inhomogeneous tuples.
+
+Note that none of the following functions are exported, since their name often collides with the equivalent functions (with different implementations) in `Base`. Some functions here provided just have an equivalent in `Base` that doesn't work for tuples at all, like `sort` or `cumsum` and `cumprod`. Some functions also provide reasonable defaults assuming that they are used for tuples of `Int`s, i.e. `TupleToolsXD.sum(()) = 0` or `TupleToolsXD.prod(()) = 1` (whereas the corresponding `Base` functions would error). This originates from the assumption that these methods are used to operate on tuples of e.g. sizes, indices or strides of multidimensional arrays.
+
+## Types
+
+```@docs
+TupleToolsXD.StaticLength
 ```
 
-# TupleToolsXD
+## Functions
 
-Documentation for [TupleToolsXD](https://github.com/PhysicsCodesLab/TupleToolsXD.jl).
-
-```@index
+```@docs
+TupleToolsXD.tail2
+TupleToolsXD.unsafe_tail
+TupleToolsXD.unsafe_front
 ```
 
-```@autodocs
-Modules = [TupleToolsXD]
+```@docs
+TupleToolsXD.getindices
+```
+
+```@docs
+TupleToolsXD.deleteat
+TupleToolsXD.insertat
+TupleToolsXD.insertafter
+TupleToolsXD.vcat
+TupleToolsXD.flatten
+```
+
+```@docs
+TupleToolsXD.sum
+TupleToolsXD.cumsum
+TupleToolsXD.diff
+TupleToolsXD.prod
+TupleToolsXD.cumprod
+```
+
+```@docs
+TupleToolsXD.minimum
+TupleToolsXD.maximum
+TupleToolsXD.findmin
+TupleToolsXD.findmax
+TupleToolsXD.argmin
+TupleToolsXD.argmax
+```
+
+```@docs
+TupleToolsXD.sort
+TupleToolsXD.sortperm
+TupleToolsXD.isperm
+TupleToolsXD.invperm
+TupleToolsXD.permute
 ```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,79 @@
+using Random, Test
 using TupleToolsXD
-using Test
 
-@testset "TupleToolsXD.jl" begin
-    # Write your tests here.
+using Base: tail, front
+
+n = 10
+
+p = randperm(n)
+ip = invperm(p)
+
+t = (p...,)
+
+@test @inferred(TupleToolsXD.argtail2(1, 2, 3, 4)) == (3, 4)
+@test @inferred(TupleToolsXD.tail2(t)) == t[3:n]
+@test @inferred(TupleToolsXD.unsafe_tail(t)) == t[2:n]
+@test @inferred(TupleToolsXD.unsafe_front(t)) == t[1:n-1]
+@test @inferred(TupleToolsXD.unsafe_tail(())) == ()
+@test @inferred(TupleToolsXD.unsafe_front(())) == ()
+@test @inferred(TupleToolsXD.vcat()) == ()
+@test @inferred(TupleToolsXD.vcat((1, 2))) == (1, 2)
+@test @inferred(TupleToolsXD.getindices(t, ())) == ()
+@test @inferred(TupleToolsXD.getindices(t, (1,2,3))) == t[1:3]
+
+@test @inferred(TupleToolsXD.deleteat((1, 2), (1, ))) == (2, )
+for i = 1:n
+    @test @inferred(TupleToolsXD.deleteat(t, i)) == (deleteat!(copy(p), i)...,)
+    @test @inferred(TupleToolsXD.insertat(t, i, (1,2,3))) == (vcat(p[1:i-1], [1,2,3], p[i+1:n])...,)
 end
+@test @inferred(TupleToolsXD.deleteat((1,2,3,4,5,6), (3,1,5))) == (2,4,6)
+@test_throws BoundsError TupleToolsXD.deleteat(t, 0)
+@test_throws BoundsError TupleToolsXD.deleteat(t, n+1)
+@test_throws BoundsError TupleToolsXD.insertat(t, 0, (1,2,3))
+@test_throws BoundsError TupleToolsXD.insertat(t, n+1, (1,2,3))
+for i = 0:n
+    @test @inferred(TupleToolsXD.insertafter(t, i, (1,2,3))) == (vcat(p[1:i], [1,2,3], p[i+1:n])...,)
+end
+@test_throws BoundsError TupleToolsXD.insertafter(t, -1, (1,2,3))
+@test_throws BoundsError TupleToolsXD.insertafter(t, n+1, (1,2,3))
+@test @inferred(TupleToolsXD.vcat((1,2,3),4,(5,),(),(6,7,8))) == (1,2,3,4,5,6,7,8)
+@test @inferred(TupleToolsXD.flatten((1,2,3),4,(5,),(),(6,7,8))) == (1,2,3,4,5,6,7,8)
+@test @inferred(TupleToolsXD.flatten((1,(2,3)),4,(5,),(),((6,),(7,(8,))))) == (1,2,3,4,5,6,7,8)
+
+@test @inferred(TupleToolsXD.sum(())) == 0
+@test @inferred(TupleToolsXD.sum((1, ))) == 1
+@test @inferred(TupleToolsXD.sum(t)) == sum(t)
+@test @inferred(TupleToolsXD.cumsum(())) == ()
+@test @inferred(TupleToolsXD.cumsum((1, ))) == (1, )
+@test @inferred(TupleToolsXD.cumsum(t)) == (cumsum(p)...,)
+@test @inferred(TupleToolsXD.prod(())) == 1
+@test @inferred(TupleToolsXD.prod((2, ))) == 2
+@test @inferred(TupleToolsXD.prod(t)) == prod(t)
+@test @inferred(TupleToolsXD.cumprod(())) == ()
+@test @inferred(TupleToolsXD.cumprod((1, ))) == (1, )
+@test @inferred(TupleToolsXD.cumprod(t)) == (cumprod(p)...,)
+
+for a in (t, (1, ))
+    @test @inferred(TupleToolsXD.findmin(a)) == findmin(a)
+    @test @inferred(TupleToolsXD.findmax(a)) == findmax(a)
+    @test @inferred(TupleToolsXD.minimum(a)) == minimum(a)
+    @test @inferred(TupleToolsXD.maximum(a)) == maximum(a)
+    @test @inferred(TupleToolsXD.argmin(a)) == argmin(a)
+    @test @inferred(TupleToolsXD.argmax(a)) == argmax(a)
+end
+
+@test @inferred(TupleToolsXD.sort((1, ))) == (1, )
+@test @inferred(TupleToolsXD.sort(())) == ()
+@test @inferred(TupleToolsXD.sort(t; rev = true)) == (sort(p; rev = true)...,)
+@test @inferred(TupleToolsXD.sort(t; rev = false)) == (sort(p; rev = false)..., )
+@test @inferred(TupleToolsXD.sortperm(t)) == (sortperm(p)...,)
+@test @inferred(TupleToolsXD.sortperm(t; rev=true)) == (sortperm(p; rev=true)..., )
+@test @inferred(TupleToolsXD.invperm(t)) == (ip...,)
+@test @inferred(TupleToolsXD.isperm(t)) == true
+@test @inferred(TupleToolsXD.isperm((1,2,1))) == false
+@test @inferred(TupleToolsXD.permute(t, t)) == (p[p]...,)
+
+@test @inferred(TupleToolsXD.vcat()) == ()
+@test @inferred(TupleToolsXD.diff(())) == ()
+@test @inferred(TupleToolsXD.diff((1, ))) == ()
+@test @inferred(TupleToolsXD.diff((1, 2, 3))) == (1, 1)
